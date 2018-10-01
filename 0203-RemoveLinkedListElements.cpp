7b6d26ed95660932145faee5c177d7b703e2dcb0
Leetcode <43736508+Ariel1986@users.noreply.github.com>
2018-10-01 18:26:45 +0800

    Create 0203-RemoveLinkedListElements.cpp

---
+++ 0203-RemoveLinkedListElements.cpp
@@ -0,0 +1,40 @@
+/*
+Remove all elements from a linked list of integers that have value val.
+
+Example:
+
+Input:  1->2->6->3->4->5->6, val = 6
+Output: 1->2->3->4->5
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(head == nullptr){ return head;}
+        ListNode dummyHead(0);
+        dummyHead.next= head;
+        
+        ListNode* pPre = &dummyHead;
+        ListNode* pCur = head;
+        while(pCur != nullptr){
+            if(pCur->val == val){
+                pPre->next = pCur->next;
+                delete pCur;
+                pCur = pPre->next;
+            }else{
+                pPre = pCur;
+                pCur = pCur->next;
+            }
+        }
+        
+        return dummyHead.next;
+    }
+};
