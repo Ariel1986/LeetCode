580e88204df3485588513a375c4eb0899c0d83ed
Leetcode <43736508+Ariel1986@users.noreply.github.com>
2018-10-02 17:39:19 +0800

    Create 119.Pascal'sTriangleII.cpp

---
+++ 119.Pascal'sTriangleII.cpp
@@ -0,0 +1,31 @@
+/*Question:
+Given an index k, return the kth row of the Pascal's triangle.
+For example, given k = 3,
+Return [1,3,3,1].
+Note:
+Could you optimize your algorithm to use only O(k) extra space?
+Tag: Array
+SP: 118
+*/
+
+/*思路:
+从后向前处理，倒数第二个元素开始，result[j] = result[j] + result[j - 1];
+*/
+
+//Code:
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        if(rowIndex < 0)
+            return {};
+        vector<int> result{1};
+        
+        for(int i = 1; i <= rowIndex; ++i)
+        {
+            result.push_back(1);
+            for(int j = i - 1; j > 0; --j)
+                result[j] = result[j] + result[j - 1];
+        }
+        return result;
+    }
+};
