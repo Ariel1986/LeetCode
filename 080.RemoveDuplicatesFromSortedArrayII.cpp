28ef297f6c6c51a013358d644063937c62e690b6
Leetcode <43736508+Ariel1986@users.noreply.github.com>
2018-10-02 17:13:53 +0800

    Create 080.RemoveDuplicatesFromSortedArrayII.cpp

---
+++ 080.RemoveDuplicatesFromSortedArrayII.cpp
@@ -0,0 +1,31 @@
+/* Question:
+Follow up for "Remove Duplicates":
+What if duplicates are allowed at most twice?
+For example,
+Given sorted array nums = [1,1,1,2,2,3],
+Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3.
+It doesn't matter what you leave beyond the new length.
+*/
+
+int removeDuplicates(std::vector<int>& nums)
+{
+    if (nums.size() <= 2) return (int)nums.size();
+    
+    int k = 1;
+    for (int i = 1, iEnd = (int)nums.size(), count = 1; i < iEnd; ++i)
+    {
+        if (nums[i] == nums[i-1])
+        {
+            count++;
+            if (count <= 2)
+                nums[k++] = nums[i];
+        }
+        else
+        {
+            nums[k++] = nums[i];
+            count = 1;
+        }
+    }
+
+    return k;
+}
