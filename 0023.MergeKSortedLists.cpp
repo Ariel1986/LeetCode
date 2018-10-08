73a8b1b4728d47291be6f56229ea151c49e9f5fe
Leetcode <43736508+Ariel1986@users.noreply.github.com>
2018-10-08 18:16:29 +0800

    Update and rename 023.MergeKSortedLists.cpp to 0023.MergeKSortedLists.cpp

--- 0023.MergeKSortedLists.cpp
+++ 0023.MergeKSortedLists.cpp
@@ -110,3 +110,48 @@ public:
         
         return dummyHead.next;
     }
+
+//code 3:
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeHelper(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil && list2 == nil {
+		return nil
+	} else if list1 == nil {
+		return list2
+	} else if list2 == nil {
+		return list1
+	} else if list1.Val < list2.Val {
+		list1.Next = mergeHelper(list1.Next, list2)
+		return list1
+	} else {
+		list2.Next = mergeHelper(list1, list2.Next)
+		return list2
+	}
+}
+
+func mergeKLists(lists []*ListNode) *ListNode {
+	if len(lists) == 0 {
+		return nil
+	}
+	for true {
+		if len(lists) == 1 {
+			break
+		}
+		var dummyLists []*ListNode
+		i := 0
+		for iEnd := len(lists) - 1; i < iEnd; i += 2 {
+			dummyLists = append(dummyLists, mergeHelper(lists[i], lists[i+1]))
+		}
+		if i == len(lists)-1 {
+			dummyLists = append(dummyLists, lists[i])
+		}
+		lists = dummyLists
+	}
+	return lists[0]
+}
