0a5016639cbd4a528ae7ab0fd962300ab1ad7753
Leetcode <43736508+Ariel1986@users.noreply.github.com>
2018-10-10 19:02:25 +0800

    Update and rename 082.RemoveDuplicatesFromSortedListII.cpp to 0082.RemoveDuplicatesFromSortedListII.cpp

--- 0082.RemoveDuplicatesFromSortedListII.cpp
+++ 0082.RemoveDuplicatesFromSortedListII.cpp
@@ -5,6 +5,38 @@ Given 1->2->3->3->4->4->5, return 1->2->5.
 Given 1->1->1->2->3, return 2->3.
 */
 
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	dummyHead := ListNode{Next: head}
+	pPre := &dummyHead
+	pCur := head
+
+	for pCur != nil {
+		bDel := false
+		for pCur != nil && pCur.Next != nil && pCur.Val == pCur.Next.Val {
+			pCur = pCur.Next
+			bDel = true
+		}
+
+		if !bDel { //无重复元素
+			pPre.Next = pCur
+			pPre = pPre.Next
+		}
+		pCur = pCur.Next
+	}
+	pPre.Next = nil
+	return dummyHead.Next
+}
+
 //Code:
 ListNode* deleteDuplicates(ListNode* head) {
     if(head == nullptr || head->next == nullptr)
